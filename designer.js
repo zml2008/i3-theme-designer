bf8094811a49fa6eb871d0df1f7a766b603cbf01
zml <zml@aoeu.xyz>
2016-11-16 20:51:48 -0800

    Deploy to GitHub Pages: 8ffbc451bd74fd257b5177ec7d09621997df01b2

---
+++ designer.js
@@ -0,0 +1,116 @@
+"use strict";
+
+// element declarations
+var colorData = {};
+var configOutputElement = document.getElementById("config-output-insert");
+var knobsElement = document.getElementById("knobs");
+var varSetRe = /set \$([^ ]+) (#[0-9A-Fa-f]{6})/;
+var suppressUpdates = false;
+
+
+// color management functions
+function updateConfig() {
+    if (suppressUpdates) {
+        return;
+    }
+    configOutputElement.value = "";
+    for (var name of Object.keys(colorData)) {
+        var data = colorData[name];
+        configOutputElement.value += "set $" + name + " " + data.color + "\n";
+        data.update(data.color)
+    }
+}
+
+// String String (hex color) (hex color) -> void
+function registerColor(name, configKey, defaultValue, updateFunc) {
+    var inputEl = document.createElement("input");
+    inputEl.id=configKey;
+    inputEl.classList.add("color");
+    inputEl.value = defaultValue;
+    var labelEl = document.createElement("label");
+    labelEl.htmlFor = inputEl.id;
+    labelEl.innerHTML = name;
+    var containerEl = document.createElement("div");
+    containerEl.appendChild(labelEl);
+    containerEl.appendChild(inputEl);
+    knobsElement.appendChild(containerEl);
+
+    colorData[configKey] = {color: defaultValue,
+        name: name,
+        update: updateFunc,
+        element: inputEl
+    };
+}
+
+function forEachElWithClass(className, func) {
+    for (var el of document.getElementsByClassName(className)) {
+        func(el);
+    }
+}
+
+// Update color data from config paste
+function parseColorData(configText) {
+    suppressUpdates = true;
+    try {
+        configText.split("\n").forEach(line => {
+            var reMatch = varSetRe.exec(line);
+            if (reMatch !== null) {
+                var param = reMatch[1];
+                var color = reMatch[2];
+                if (colorData[param]) {
+                    var data = colorData[param];
+                    data.color = color;
+                    data.update(data.color);
+                    data.element.value = data.color;
+                }
+            } else if (line != "") { // the last line is empty so never matches
+                console.log("Line " + line + " was not a valid variable line!");
+            }
+        });
+    } finally {
+        suppressUpdates = false;
+    }
+}
+configOutputElement.oninput = ev => parseColorData(ev.srcElement.value)
+
+
+
+
+// initialize color pickers
+registerColor("Focused Window Foreground", "focused_fg", "#222222", 
+        col => forEachElWithClass("focused", el => el.style.color = col));
+registerColor("Focused Window Background", "focused_bg", "#eeeeee", 
+        col => forEachElWithClass("focused", el => el.style.backgroundColor = col));
+registerColor("Focused Inactive Window Foreground", "focused_inactive_fg", "#222222", 
+        col => forEachElWithClass("focused-inactive", el => el.style.color = col));
+registerColor("Focused Inactive Window Background", "focused_inactive_bg", "#dddddd", 
+        col => forEachElWithClass("focused-inactive", el => el.style.backgroundColor = col));
+registerColor("Unfocused Window Foreground", "unfocused_fg", "#999999", 
+        col => forEachElWithClass("unfocused", el => el.style.color = col));
+registerColor("Unfocused Window Background", "unfocused_bg", "#333333", 
+        col => forEachElWithClass("unfocused", el => el.style.backgroundColor = col));
+registerColor("Urgent Window Foreground", "urgent_fg", "#ffffff", 
+        col => forEachElWithClass("urgent", el => el.style.color = col));
+registerColor("Urgent Window Background", "urgent_bg", "#901010", 
+        col => forEachElWithClass("urgent", el => el.style.backgroundColor = col));
+registerColor("Bar Background", "bar_bg", "#111111", 
+        col => document.getElementById("bar").style.backgroundColor = col);
+
+
+// color picker init
+var colors = jsColorPicker('input.color', {
+    customBG: '#222',
+    readOnly: false,
+    // patch: false,
+    init: function(elm, colors) { // colors is a different instance (not connected to colorPicker)
+        elm.style.backgroundColor = elm.value;
+        elm.style.color = colors.rgbaMixCustom.luminance > 0.22 ? '#222' : '#ddd';
+        colorData[elm.id].color = "#" + colors.HEX;
+        updateConfig();
+    },
+    displayCallback: function(colors, mode, options) {
+        var elm = options.input;
+        colorData[elm.id].color = "#" + colors.HEX;
+        updateConfig();
+    }
+});
